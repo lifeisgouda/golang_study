7234df1fc1f1c6af1cce47f9acce7ca078ce4f15
lifeisgouda <dahaejeon@gmail.com>
2019-10-16 21:57:30 +0900

    study note

---
+++ src/section2/variable1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 변수는 기본 초기화 과정 필요
+	// 정수 타입: 0, 실수(소수점): 0.0, 문자열: "", Boolean: true, false
+
+	// var a int     // 선언만 하고 초기화 안하면 에러 발생
+	// var b string
+	// var c, d, e int
+	// var f, g, h int = 1, 2, 3
+	// var i float32 = 11.4
+	// var j string = "Hi, Golang!"
+	var k = 4.75
+	// var m = true
+
+	fmt.Println("k: ", k)
+
+}
